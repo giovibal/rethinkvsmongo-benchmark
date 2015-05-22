9685b68116e171be5e73efc8cdf85478db9707ee
martinrusev <martin@amon.cx>
2015-05-22 17:44:44 +0300

    Add requirements, updates

--- benchmark.py
+++ benchmark.py
@@ -1,34 +1,53 @@
 #! /home/martin/.virtualenvs/rethinkmongo/bin/python
 import rethinkdb as r
 from pymongo import MongoClient
-from pymongo import DESCENDING, ASCENDING
+import pymongo
 import random
 import time
 
-import calendar
-from datetime import datetime
-
-def unix_utc_now():
-	d = datetime.utcnow()
-	_unix = calendar.timegm(d.utctimetuple())
-
-	return _unix
+COLOR_GREEN = '\033[92m'
+END_COLORED_LINE = '\033[0m'
 
 database_name = 'rethinkvsmongo'
 table_name = 'metrics'
+unix_timestamp = 1432294074
+total_operations = 100000
 
+def random_timestamp():
+	return random.randrange(unix_timestamp, unix_timestamp+100000, 1)
+
+def random_int():
+	return random.randint(100, 300)
+
+
+def timing(f):
+	def wrap(*args):
+		time1 = time.time()
+		ret = f(*args)
+		time2 = time.time()
+		total = (time2-time1)*1000.0
+
+		message = "op:{0}/time:{1:.2f}ms".format(total_operations,
+		total)
+
+		print "{function} took \n{color}{message}{end}".format(
+			function=f.func_name,
+			color=COLOR_GREEN,
+		 	message=message,
+			end=END_COLORED_LINE)
+		return ret
+	return wrap
 
 mongo_connection = MongoClient(host='mongodb://localhost')
 mongo_db = mongo_connection[database_name]
 mongo_collection = mongo_db[table_name]
-mongo_collection.ensure_index([('last_update', DESCENDING)], background=True)
+mongo_collection.ensure_index([('last_update', pymongo.DESCENDING)], background=True)
 
 r.connect("localhost", 28015, db=database_name).repl()
 # r.db_create(database).run()
 # r.db(database_name).table_create("metrics").run()
 # r.table(table_name).index_create('last_update').run()
 
-now = unix_utc_now()
 metrics_document = {
 	"table_name" : "django_session",
 	"cumulative_pct_reads" : 69.8599999999999994,
@@ -39,44 +58,42 @@ metrics_document = {
 	"size" : 1
 }
 
+@timing
+def find_benchmark_mongodb():
+	for i in range(0, total_operations):
+		mongo_collection.find({"last_update": {"$in": [random_timestamp()]}})
 
-
-
-
+@timing
+def find_benchmark_rethinkdb():
+	for i in range(0, total_operations):
+		r.table(table_name).filter({"last_update":random_timestamp()})
+@timing
 def insert_benchmark_mongodb():
-	for i in range(0, 100000):
-		try:
-			del metrics_document['_id']
-		except:
-			pass
-		metrics_document['last_update'] = now+i
-		mongo_collection.insert(metrics_document)
-
-def insert_benchmark_rethink():
-	for i in range(0, 100000):
-		metrics_document['last_update'] = now+i
+	for i in range(0, total_operations):
+		data = metrics_document.copy()
+		data['last_update'] = unix_timestamp+i
+		mongo_collection.insert(data)
+@timing
+def insert_benchmark_rethinkdb():
+	for i in range(0, total_operations):
+		metrics_document['last_update'] = unix_timestamp+i
 		r.table(table_name).insert(metrics_document, durability='hard').run()
-
+@timing
 def update_benchmark_mongodb():
-	# Last document timestamp
-	# timestamp =  mongo_collection.find_one(sort=[ ("last_update", DESCENDING) ]) #1432382558
-	#
-	# last_update = timestamp['last_update']
-	# last_twenty_thousand = last_update-20000 # result - 1432362558
-	# print last_twenty_thousand
-	random_timestamp = random.randrange(1432282559, 1432382558, 1)
-	data = {'size': 145, 'reads': 12, 'cache_hit_rate': 100}
-	for i in range(0, 100000):
-		mongo_collection.update({"last_update": {"$in": [random_timestamp]}}, {"$set": data})
+	data = {'size': random_int(), 'reads': random_int(), 'cache_hit_rate': random_int()}
+	for i in range(0, total_operations):
+		mongo_collection.update({"last_update": {"$in": [random_timestamp()]}}, {"$set": data})
 
+@timing
 def update_benchmark_rethinkdb():
-	rethink_cursor = r.table(table_name).order_by(index=r.desc('last_update')).limit(1).run()
+	data = {'size': random_int(), 'reads': random_int(), 'cache_hit_rate': random_int()}
 
-	for result in rethink_cursor:
-		print result['last_update']
+	for i in range(0, 10):
+		print random_timestamp()
+		r.table(table_name).filter({"last_update":random_timestamp()}).update(data,
+		durability='soft').run()
 
 # Benchmark
-start = time.time()
-update_benchmark_rethinkdb()
-end = time.time()
-print end - start
+for i in range(0, 3):
+	find_benchmark_mongodb()
+	find_benchmark_rethinkdb()
